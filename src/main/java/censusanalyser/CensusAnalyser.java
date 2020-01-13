f18a5fc65a5a3da47ae87f3a5019f83465e4eb21
Prashant2411 <prashantbedi241197@gmail.com>
2020-01-13 12:51:23 +0530

    Refactored: Combined DAO classes for Indian and US census data

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -15,17 +15,17 @@ import static java.nio.file.Files.newBufferedReader;
 
 
 public class CensusAnalyser {
-    List<IndianCensusDAO> censusList = null; //For DAO-Data Access Object
+    List<CensusDataDAO> censusList = null; //For DAO-Data Access Object
     List<IndiaCensusCSV> censusCSVList = null;
     List<CSVStatesDAO> stateCodeList = null; //For DAO-Data Access Object
     List<CSVStates> stateCSVList = null;
-    List<USCensusDAO> usCensusList = null; //For DAO-Data Access Object
+    List<CensusDataDAO> usCensusList = null; //For DAO-Data Access Object
     List<USCensus> usCensusCSVList = null;
 
     public CensusAnalyser() {
-        this.censusList = new ArrayList<IndianCensusDAO>();
+        this.censusList = new ArrayList<CensusDataDAO>();
         this.stateCodeList = new ArrayList<CSVStatesDAO>();
-        this.usCensusList = new ArrayList<USCensusDAO>();
+        this.usCensusList = new ArrayList<CensusDataDAO>();
 
     }
 
@@ -34,7 +34,7 @@ public class CensusAnalyser {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
             censusCSVList = csvBuilder.getCSVFileInList(reader, IndiaCensusCSV.class);
             for (int i=0;i<censusCSVList.size();i++)
-                this.censusList.add(new IndianCensusDAO(censusCSVList.get(i)));
+                this.censusList.add(new CensusDataDAO(censusCSVList.get(i)));
             return censusList.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
@@ -78,7 +78,7 @@ public class CensusAnalyser {
             if(censusList.size()==0 || censusList == null)
                 throw new CensusAnalyserException("Invalid File",
                         CensusAnalyserException.ExceptionType.NULL_EXCEPTION);
-            censusList = censusList.stream().sorted(Comparator.comparing(IndianCensusDAO::getStateDAO)).collect(Collectors.toList());
+            censusList = censusList.stream().sorted(Comparator.comparing(CensusDataDAO::getStateDAO)).collect(Collectors.toList());
             String censusCodeJson = new Gson().toJson(this.censusList);
             return censusCodeJson;
         } catch (IllegalStateException e) {
@@ -112,7 +112,7 @@ public class CensusAnalyser {
             if(censusList.size()==0 || censusList == null)
                 throw new CensusAnalyserException("Invalid File",
                         CensusAnalyserException.ExceptionType.NULL_EXCEPTION);
-            censusList = censusList.stream().sorted(Comparator.comparing(IndianCensusDAO::getPopulationDAO).reversed()).collect(Collectors.toList());
+            censusList = censusList.stream().sorted(Comparator.comparing(CensusDataDAO::getPopulationDAO).reversed()).collect(Collectors.toList());
             String censusJson = new Gson().toJson(this.censusList);
             return censusJson;
         } catch (IllegalStateException e) {
@@ -129,7 +129,7 @@ public class CensusAnalyser {
             if(censusList.size()==0 || censusList == null)
                 throw new CensusAnalyserException("Invalid File",
                         CensusAnalyserException.ExceptionType.NULL_EXCEPTION);
-            censusList = censusList.stream().sorted(Comparator.comparing(IndianCensusDAO::getDensityPerSqKmDAO).reversed()).collect(Collectors.toList());
+            censusList = censusList.stream().sorted(Comparator.comparing(CensusDataDAO::getDensityPerSqKmDAO).reversed()).collect(Collectors.toList());
             String censusJson = new Gson().toJson(this.censusList);
             return censusJson;
         } catch (IllegalStateException e) {
@@ -146,7 +146,7 @@ public class CensusAnalyser {
             if(censusList.size()==0 || censusList == null)
                 throw new CensusAnalyserException("Invalid File",
                         CensusAnalyserException.ExceptionType.NULL_EXCEPTION);
-            censusList = censusList.stream().sorted(Comparator.comparing(IndianCensusDAO::getAreaInSqkmDAO).reversed()).collect(Collectors.toList());
+            censusList = censusList.stream().sorted(Comparator.comparing(CensusDataDAO::getAreaInSqkmDAO).reversed()).collect(Collectors.toList());
             String censusJson = new Gson().toJson(this.censusList);
             return censusJson;
         } catch (IllegalStateException e) {
@@ -163,7 +163,7 @@ public class CensusAnalyser {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
             usCensusCSVList = csvBuilder.getCSVFileInList(reader, USCensus.class);
             for (int i = 0; i < usCensusCSVList.size(); i++)
-                this.usCensusList.add(new USCensusDAO(usCensusCSVList.get(i)));
+                this.usCensusList.add(new CensusDataDAO(usCensusCSVList.get(i)));
             return usCensusList.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
---
+++ src/main/java/censusanalyser/CensusDataDAO.java
@@ -0,0 +1,50 @@
+package censusanalyser;
+
+public class CensusDataDAO {
+    private int areaInSqkm;
+    private String state;
+    private int population;
+    private int densityPerSqKm;
+    public double Housing_Density;
+    public double Land_area;
+    public double Water_area;
+    public int Housing_units;
+    public String State_Id;
+
+    public CensusDataDAO(IndiaCensusCSV indianCensusCSV) {
+    state = indianCensusCSV.state;
+    areaInSqkm = indianCensusCSV.areaInSqKm;
+    population = indianCensusCSV.population;
+    densityPerSqKm = indianCensusCSV.densityPerSqKm;
+    }
+
+    public CensusDataDAO(USCensus usCensus) {
+        state = usCensus.State;
+        areaInSqkm = (int) usCensus.Total_area;
+        population = usCensus.Population;
+        densityPerSqKm = (int) usCensus.Population_Density;
+        Housing_Density = usCensus.Housing_Density;
+        Land_area = usCensus.Land_area;
+        Water_area = usCensus.Water_area;
+        Housing_units = usCensus.Housing_units;
+        State_Id = usCensus.State_Id;
+    }
+
+    public String getStateDAO() {
+        return state;
+    }
+
+    public int getPopulationDAO() {
+        return population;
+    }
+
+    public int getDensityPerSqKmDAO() {
+        return densityPerSqKm;
+    }
+
+    public int getAreaInSqkmDAO() {
+        return areaInSqkm;
+    }
+
+
+}
